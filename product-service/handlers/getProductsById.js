eb74a9d08a4725f769e88f1886c642d35e86949c
Pavel Lichmaniuk <Pavel Lichmaniuk>
2021-03-14 11:18:19 +0300

    update handlers

--- product-service/handlers/getProductsById.js
+++ product-service/handlers/getProductsById.js
@@ -1,15 +1,29 @@
 import { productList } from '../productList';
 
 export const getProductsById = async (event) => {
-  const productId = event.pathParameters.productId;
-  const result = productList.find(product => product.id === productId);
-
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    },
-    body: JSON.stringify(result)
-  };
+  try {
+    const productId = event.pathParameters.productId;
+    const result = productList.find(product => product.id === productId);
+    if (!result) {
+      throw Error;
+    }
+  
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify(result)
+    };
+  } catch (err) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify('Product not found')
+    };
+  }
 };
--- product-service/handlers/getProductsList.js
+++ product-service/handlers/getProductsList.js
@@ -1,12 +1,13 @@
 import { productList } from '../productList';
 
 export const getProductsList = async (event) => {
+  const result = JSON.stringify(productList);
   return {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
-    body: JSON.stringify(productList)
+    body: result
   };
 };
