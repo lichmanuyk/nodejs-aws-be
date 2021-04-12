11f73422b41f4274ce5d22ca02602be38556dce2
Pavel Lichmaniuk <PLichmaniuk@slb.com>
2021-04-12 19:49:05 +0300

    update lambdas

--- product-service/handlers/getProductsById.js
+++ product-service/handlers/getProductsById.js
@@ -1,10 +1,27 @@
-import { productList } from '../productList';
+import { Client } from 'pg';
+
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+const dbOptions = {
+  host: PG_HOST,
+  port: PG_PORT,
+  database: PG_DATABASE,
+  user: PG_USERNAME,
+  password: PG_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  connectionTimeoutMillis: 5000,
+};
 
 export const getProductsById = async (event) => {
+  const client = new Client(dbOptions);
+  await client.connect();
+
   try {
     const productId = event.pathParameters.productId;
-    const result = productList.find(product => product.id === productId);
-    if (!result) {
+    const { rows: products } = await client.query(`select * from products left join stocks on products.id = stocks.product_id and products.id = '${productId}'`);
+
+    if (!products) {
       throw Error;
     }
   
@@ -14,7 +31,7 @@ export const getProductsById = async (event) => {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true,
       },
-      body: JSON.stringify(result)
+      body: JSON.stringify(products[0])
     };
   } catch (err) {
     return {
@@ -23,7 +40,9 @@ export const getProductsById = async (event) => {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true,
       },
-      body: JSON.stringify('Product not found')
+      body: JSON.stringify('Product not found', err)
     };
+  } finally {
+    client.end();
   }
 };
--- product-service/handlers/getProductsList.js
+++ product-service/handlers/getProductsList.js
@@ -1,13 +1,43 @@
-import { productList } from '../productList';
+import { Client } from 'pg';
+
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+const dbOptions = {
+  host: PG_HOST,
+  port: PG_PORT,
+  database: PG_DATABASE,
+  user: PG_USERNAME,
+  password: PG_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  connectionTimeoutMillis: 5000,
+};
 
 export const getProductsList = async (event) => {
-  const result = JSON.stringify(productList);
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    },
-    body: result
-  };
+  const client = new Client(dbOptions);
+  await client.connect();
+
+  try {
+    const { rows: products } = await client.query('select * from products left join stocks on products.id = stocks.product_id');
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: products
+    };
+  } catch (err) {
+    console.log('Error during database request executing:', err);
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+  } finally {
+    client.end();
+  }
 };
