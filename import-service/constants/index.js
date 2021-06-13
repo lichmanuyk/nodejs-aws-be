728c0f66445ed2b6e44a5a1178d92fd10288c226
Pavel Lichmaniuk <PLichmaniuk@slb.com>
2021-06-13 13:07:12 +0300

    update import service to send received and parsed data from CSV to SQS service

--- import-service/constants/index.js
+++ import-service/constants/index.js
@@ -1,2 +1,3 @@
 export const BUCKET_NAME = "import-service-csv-bucket";
 export const CATALOG_PATH = "uploaded/";
+export const SQS_URL = "https://sqs.eu-west-1.amazonaws.com/072657732908/catalogItemsQueue";
--- import-service/handlers/importFileParser.js
+++ import-service/handlers/importFileParser.js
@@ -1,11 +1,28 @@
 import AWS from 'aws-sdk';
 import csvParser from "csv-parser";
-import { BUCKET_NAME } from "../constants/index";
+import { BUCKET_NAME, SQS_URL } from "../constants/index";
 import createResponse from "../utils/createResponse";
+
+const sentDataToQueue = (sqs, data) => {
+    return new Promise((resolve, reject) => {
+        sqs.sendMessage({
+            QueueUrl: SQS_URL,
+            MessageBody: JSON.stringify(data)
+        }, (err, result) => {
+            if (err) {
+                console.log('error while send', err);
+                reject(err);
+            }
+            console.log('sent data', result);
+            resolve();
+        })
+    })
+}
 
 export const importFileParser = async (event) => {
     try {
         const s3 = new AWS.S3({region: 'eu-west-1'});
+        const sqs = new AWS.SQS({region: 'eu-west-1'});
         const promise = new Promise((res, rej) => {
             const fileName = event.Records[0].s3.object.key;
             const params = {
@@ -17,7 +34,9 @@ export const importFileParser = async (event) => {
 
             s3Stream
                 .pipe(csvParser())
-                .on('data', (data) => {console.log("Data", data)})
+                .on('data', async (data) => {
+                    await sentDataToQueue(sqs, data);
+                })
                 .on('error', (error) => {console.log("Error", error); rej();})
                 .on('end', async () => {
                     console.log('end file processing');
